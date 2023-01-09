2a096689ad9170a300a60f831c128f81234bb754
brightboost <info@brightboost.nl>
2023-01-09 16:27:58 +0100

    hobbies

--- src/com/company/OefeningLoops.java
+++ src/com/company/OefeningLoops.java
@@ -3,12 +3,14 @@ package com.company;
 public class OefeningLoops {
     public static void main(String[] args) {
         // maak een methode die een getal als input neemt en vervolgens een getallenreeks van 0 naar het inputgetal afdrukt naar de console
+        // breid de methode uit met een parameter voor het printen van alle getallen, alleen de even, en alleen de oneven (gebruik een if statement)
         maakGetallenReeks(20, "alles");
         maakGetallenReeks(20, "even");
         maakGetallenReeks(20, "oneven");
 
-        // breid de methode uit met een parameter voor het printen van alle getallen, alleen de even, en alleen de oneven (gebruik een if statement)
         // maak een methode die een array van hobbies als input neemt, en vervolgens voor iedere hobby print "oh wat leuk, xxx" (bijv oh wat leuk, coderen)
+        String[] bezigheden = {"Java coderen", "C# coderen", "PHP coderen"};
+        printHobbies(bezigheden);
     }
 
     public static void maakGetallenReeks(int max, String modus) {
@@ -26,5 +28,11 @@ public class OefeningLoops {
             System.out.println(teller);
         }
 
+    }
+
+    public static void printHobbies(String[] hobbies) {
+        for(int i = 0; i < hobbies.length; i++) {
+            System.out.println("Oh leuk, " + hobbies[i]);
+        }
     }
 }
